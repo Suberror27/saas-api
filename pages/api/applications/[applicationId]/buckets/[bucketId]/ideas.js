2d8e4c5cff1254bd616aa5b33713675cfb3ce296
Suberror27 <abrahamct27@gmail.com>
2025-01-12 15:43:34 -0600

    Somewhat completed step 3.2, have questions for Kevin

---
+++ pages/api/applications/[applicationId]/buckets/[bucketId]/ideas.js
@@ -0,0 +1,48 @@
+import { db } from '@/lib/firebaseAdmin';
+
+export default async function postHandler(req, res) {
+    // Making sure the method is always POST else return error message
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+
+    const applicationId = req.query.applicationId;
+    const bucketId = req.query.bucketId;
+    const {text, metadata} = req.body;
+    
+    try {
+        if (!metadata.createdBy || !metadata.createdAt) {
+            return res.status(400).json({message: "Need createdBy and createdAt data"});
+        } else {
+            const docRef = db.collection("Applications").doc(applicationId).collection("Buckets").doc(bucketId).collection("Ideas").doc();
+
+            const newIdea = await docRef.set({
+                text,
+                metadata 
+            });
+    
+            return res.status(200).json(
+                {
+                    ideaId: docRef.id, 
+                    text: text, 
+                    metadata: {
+                        createdBy: metadata.createdBy, 
+                        createdAt: metadata.createdAt, 
+                        lastPickedAt: "timestamp", 
+                        timesPicked: "timestamp", 
+                        pickHistory: [
+                            {
+                                userId: "string", 
+                                pickedAt: "timestamp"
+                            }
+                        ]
+                    } 
+                }
+            )
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return res.status(500).json({ error: 'Failed to add Idea into Bucket' });
+    }
+}
+
