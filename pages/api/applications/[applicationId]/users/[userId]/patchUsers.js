543b16d5a92263ee193ae97b27d54def48679afa
Suberror27 <abrahamct27@gmail.com>
2025-01-12 14:25:16 -0600

    Completed step 2.3

---
+++ pages/api/applications/[applicationId]/users/[userId]/patchUsers.js
@@ -0,0 +1,53 @@
+import { db } from '@/lib/firebaseAdmin';
+
+export default async function patchHandler(req, res) {
+    // Making sure the method is always POST else return error message
+    if (req.method !== 'PATCH') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+
+    const applicationId = req.query.applicationId;
+    const userId = req.query.userId;
+    const {name, roles} = req.body;
+   
+    if(!userId || !applicationId) {
+        return res.status(400).json({error: "userId and applicationId are a MUST, at least name or roles data is required"})
+    } else {
+        if (!name && !roles) {
+            return res.status(404).json({message: "At least name or roles needed to update users information"})
+        } else {
+            try {
+
+                // Reference to the Users subcollection under the specific ApplicationId document
+                const userRef = db.collection("Applications").doc(applicationId).collection("Users").doc(userId);
+    
+                // Check if the user document exists
+                const userDoc = await userRef.get();
+    
+                if (!userDoc.exists) {
+                    return res.status(404).json({ error: "User not found under the specified applicationId" });
+                } else {
+                    const updateInformation = {};
+                    if (name !== undefined) {
+                        updateInformation.name = name;
+                    } else {
+                        updateInformation.name = userDoc.data().name;
+                    }
+                    if (roles !== undefined) {
+                        updateInformation.roles = roles;
+                    } else {
+                        updateInformation.roles = userDoc.data().roles;
+                    }
+    
+                    await userRef.update(updateInformation);
+    
+                    return res.status(200).json({ success: true, message: "User data updated succesfully", updatedInfo: {userId: userId, name: name ? name : updateInformation.name = userDoc.data().name, roles: roles ? roles : userDoc.data().roles} });
+                }
+            } catch (error) {
+                console.error("Error updating user:", error);
+                return res.status(500).json({ error: 'Failed to patch users information' });
+            }
+        }
+    }
+}
+
--- pages/api/applications/[applicationId]/users/postUsers.js
+++ pages/api/applications/[applicationId]/users/postUsers.js
@@ -7,23 +7,23 @@ export default async function postHandler(req, res) {
     }
 
     const applicationId = req.query.applicationId;
-    const { userId, name, role } = req.body;
+    const { userId, name, roles } = req.body;
 
-    if(!name || !role || !userId || !applicationId) {
-        return res.status(400).json({error: "Name, Role, userId, and ApplicationId fields are required"})
+    if(!name || !roles || !userId || !applicationId) {
+        return res.status(400).json({error: "Name, Roles, userId, and ApplicationId fields are required"})
     } else {
         try {
-            console.log(typeof(role));
+            console.log(typeof(roles));
             // Reference to the Users subcollection under the specific ApplicationId document
             const userRef = db.collection("Applications").doc(applicationId).collection("Users").doc(userId);
             
             // Create the new user document
             await userRef.set({
                 name,
-                role
+                roles
             });
 
-            return res.status(200).json({ success: true, message: "User created successfully", newUser: {name: name, role: role, userId: userId} });
+            return res.status(200).json({ success: true, message: "User created successfully", newUser: {name: name, roles: roles, userId: userId} });
         } catch (error) {
             console.error('Error creating application:', error);
             return res.status(500).json({ error: 'Failed to create New User' });
